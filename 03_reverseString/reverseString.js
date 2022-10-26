cb8d764067642ddc4b418396a0bfc6b1bfc73d12
MountainDragon <dr.katmorales@gmail.com>
2022-10-26 16:56:16 -0600

    Fix the code to pass all the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+let strArray = [];
+strArray = str.split("");
+let reversedArr = [...strArray];
+reversedArr.reverse();
+let reversedStr = reversedArr.join("")
+return reversedStr;
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
