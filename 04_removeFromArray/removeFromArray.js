219a95604bd40177e83c1fb24947e7d407214fbf
MountainDragon <97689355+MountainDragon@users.noreply.github.com>
2022-11-02 15:29:50 -0700

    Fix the code to pass all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
+let arr = arguments[0];
 
+let removeArr = [];
+for(let i=1; i<arguments.length; i++){
+    removeArr.push(arguments[i]);
+}
+
+const result = arr.filter(num => removeArr.indexOf(num)<0);
+return result;
 };
 
+
+//removeFromArray([1, 3, 4, 6, 7], 4, 6)
 // Do not edit below this line
 module.exports = removeFromArray;
