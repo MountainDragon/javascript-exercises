7d7e95b2935b2dade41033a23485027100d090bc
MountainDragon <97689355+MountainDragon@users.noreply.github.com>
2022-11-02 16:00:46 -0700

    Fix the code to pass all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
 const sumAll = function() {
+let argArr = [...arguments];
+argArr.sort(function(a,b){return a-b})
+const min = argArr[0];
+const max = argArr[1];
+
+let result;
+
+if(min >= 0 && max >= 0 && typeof(min) === "number" && typeof(max) === "number"){
+    let sum = 0;
+    for(let i=min; i<=max; i++){
+        sum+=i;
+    }
+    result = sum;
+}else{
+    result = "ERROR";
+}
+return result;
+
+
+
 
 };
-
+sumAll("99", 5);
 // Do not edit below this line
 module.exports = sumAll;
