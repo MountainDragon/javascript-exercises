01a498d41e43e2c6e917bd341b896fb408832cad
MountainDragon <97689355+MountainDragon@users.noreply.github.com>
2022-11-02 17:31:43 -0700

    Fix the code to pass all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+if(year%4===0){
+    if(year%100===0){
+        if(year%400===0){
+            return true;
+        }else{return false;}
+    }else{
+        return true;
+    }
+}else{
+    return false;
+}
 };
 
+leapYears(1600);
+leapYears(34992);
+leapYears(1996);
 // Do not edit below this line
 module.exports = leapYears;
