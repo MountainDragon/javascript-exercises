04215ee83f1cfd10648e9c59bbc41e07536f613e
MountainDragon <97689355+MountainDragon@users.noreply.github.com>
2022-11-02 17:53:05 -0700

    Fix code to pass al the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel;
+  cel = (fah - 32) * (5/9);
+  return Number(cel.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fah;
+  fah = cel * (9/5) + 32;
+  return Number(fah.toFixed(1));
 };
+
+ftoc(-100)
 
 // Do not edit below this line
 module.exports = {
